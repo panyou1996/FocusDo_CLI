1f557aef31badf2c69a18668971f0649d468ab8c
you pan <panyouxgxg@gmail.com>
2025-09-26 07:00:04 +0000

    动画感觉还是像之前这样：先出现在了顶部，然后跳到底部从底部滑动到上面，请问是和其他的动画有冲突或重叠吗，比如背景的动画等

--- src/app/(main)/template.tsx
+++ src/app/(main)/template.tsx
@@ -1,8 +1,29 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
+import * as React from 'react';
+
+const MODAL_PATHS = [
+    '/add-task', 
+    '/journal/new', 
+    '/add-list',
+    '/settings/appearance',
+    '/settings/profile'
+];
+const EDIT_TASK_REGEX = /^\/edit-task\/.+/;
 
 export default function Template({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  const isModalPage = MODAL_PATHS.includes(pathname) || EDIT_TASK_REGEX.test(pathname);
+
+  if (isModalPage) {
+    // For modal pages, we don't want the default template animation.
+    // The page itself will handle its entry/exit animation.
+    return <div>{children}</div>;
+  }
+
   return (
     <motion.div
       initial={{ y: 16, opacity: 0 }}
